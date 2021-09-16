95bf719c410f6808b68d8eae76d13e49a80c521c
Jason Yuan <jason.yuan869@gmail.com>
2021-09-16 09:43:31 -0400

    Update main() for tutorial-servo-can-control

--- test-apps/tutorial-servo-pot-control/src/main.cpp
+++ test-apps/tutorial-servo-pot-control/src/main.cpp
@@ -1,5 +1,16 @@
 //
 // Created by jason on 2021-09-15.
 //
+#include "mbed.h"
 
-#include "main.h"
+AnalogIn potVoltageIn(PA_0);
+PwmOut servoPwmOut(PA_1);
+
+int main() {
+
+  servoPwmOut.period_ms(20);
+
+  while(1) {
+    servoPwmOut.pulsewidth((1 + potVoltageIn.read()/3.3) / 1000);
+  }
+}
